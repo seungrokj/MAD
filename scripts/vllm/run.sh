a7b6848cac93585df23b94220e0e45b52e413360
seungrokjung <seungrok.jung@amd.com>
2025-04-14 06:35:47 +0000

    nv
    Signed-off-by: seungrokjung <seungrok.jung@amd.com>

--- scripts/vllm/run.sh
+++ scripts/vllm/run.sh
@@ -26,7 +26,7 @@
 #################################################################################
 set -ex
 
-if [[ "$MAD_SYSTEM_GPU_ARCHITECTURE" != *"gfx94"* ]] or [[ "$MAD_SYSTEM_GPU_ARCHITECTURE" != *"H200"* ]] or [[ "$MAD_SYSTEM_GPU_ARCHITECTURE" != *"H100"* ]] ; then 
+if [[ "$MAD_SYSTEM_GPU_ARCHITECTURE" != *"gfx94"* ]] || [[ "$MAD_SYSTEM_GPU_ARCHITECTURE" != *"H200"* ]] || [[ "$MAD_SYSTEM_GPU_ARCHITECTURE" != *"H100"* ]] ; then 
     echo "Unsuported GPU arch detected, please use supported MI300X, H200, H100 GPUs \n"
     exit 1
 fi
