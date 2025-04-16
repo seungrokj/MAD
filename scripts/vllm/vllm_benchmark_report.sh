992bec41a026c48e860b367a68ff18174f0b7b4d
seungrokjung <seungrok.jung@amd.com>
2025-04-16 14:13:37 +0000

    ll4 online
    Signed-off-by: seungrokjung <seungrok.jung@amd.com>

--- scripts/vllm/vllm_benchmark_report.sh
+++ scripts/vllm/vllm_benchmark_report.sh
@@ -231,7 +231,7 @@ elif [ "$scenario" == "online_accuracy" ]; then
         wait_for_server 8080
 
         lm_eval --model local-completions --model_args model=$MODEL_DIR,base_url=http://0.0.0.0:8080/v1/completions,num_concurrent=10,max_retries=3 --tasks mmlu_pro --limit 100  2>&1 | tee -a ${LOG_sum}.log
-        lm_eval --model local-completions --model_args model=$MODEL_DIR,base_url=http://0.0.0.0:8080/v1/completions,num_concurrent=10,max_retries=3 --tasks gpqa_diamond_cot_zeroshot --apply_chat_template  2>&1 | tee -a ${LOG_sum}.log
+        #lm_eval --model local-completions --model_args model=$MODEL_DIR,base_url=http://0.0.0.0:8080/v1/completions,num_concurrent=10,max_retries=3 --tasks gpqa_diamond_cot_zeroshot --apply_chat_template  2>&1 | tee -a ${LOG_sum}.log
         lm_eval --model local-completions --model_args model=$MODEL_DIR,base_url=http://0.0.0.0:8080/v1/completions,num_concurrent=10,max_retries=3 --tasks gsm8k  2>&1 | tee -a ${LOG_sum}.log
     done < <(tail -n +2 $CONFIG)
 fi
