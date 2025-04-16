78c9ce1751eba4e8d3fc78a27733866ecdd5b850
seungrok.jung <seungrok.jung@amd.com>
2025-04-16 11:09:01 +0000

    ll4 fp8
    Signed-off-by: seungrok.jung <seungrok.jung@amd.com>

--- scripts/vllm/vllm_benchmark_report.sh
+++ scripts/vllm/vllm_benchmark_report.sh
@@ -58,9 +58,6 @@ tag="vllm_ll4"
 CON="16 32 64 128"
 ISL_OSL=("1000:1000" "5000:1000" "10000:1000" "3200:800" "2000:150")
 
-CON="16"
-ISL_OSL=("1000:1000")
-
 if [[ "$MAD_SYSTEM_GPU_ARCHITECTURE" == *"gfx94"* ]] ; then 
     CONFIG="online_config_rocm.csv"
     export VLLM_ROCM_FP8_PADDING=0 
